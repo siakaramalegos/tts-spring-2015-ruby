d40fc97819cd1871bffd08e735bd3b86b841ae85
Sia <siakaramalegos@gmail.com>
2015-04-23 14:30:31 -0500

    Added arrays in-class practice notes

---
+++ pre/arrays.rb
@@ -0,0 +1,35 @@
+my_first_array = []
+my_second_array = Array.new
+
+# puts "my_first_array is #{my_first_array}"
+# puts "my_second_array is #{my_second_array}"
+
+my_first_array[0] = "first item"
+my_first_array[1] = 2.0
+my_first_array[2] = 3
+# puts "my_first_array is #{my_first_array}"
+
+my_array = ["apple", "orange", "banana", "monkey"]
+# puts "my_array is #{my_array}"
+# puts "The zeroth index of my_array is #{my_array[0]}"
+# puts "The -1 index of my_array is #{my_array[-1]}"
+# puts "The 2 index of my_array is #{my_array[2]}"
+
+my_first_array[3] = ["fourth", 4, 4.0]
+# puts "my_first_array is #{my_first_array}"
+# puts "The zeroth item of the third (index = 3) item of my_first_array is #{my_first_array[3][0]}"
+
+dogs = []
+dogs.push("Priscilla")
+dogs << "Harry"
+dogs.push("foster pup")
+dogs.pop
+dogs << "cat"
+dogs.reverse
+dogs.reverse!
+dogs.reverse!
+dogs.pop
+x = dogs.length
+
+puts "My dogs are #{dogs}.  I have #{x} dogs."
+puts "My dogs in reverse are #{dogs.reverse}"
