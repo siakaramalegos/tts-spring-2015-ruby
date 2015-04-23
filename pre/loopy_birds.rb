78954b2b6cb86d6536c7b4574c08f6e18ae80586
Sia <siakaramalegos@gmail.com>
2015-04-23 13:40:55 -0500

    Added loops in-class exercise

---
+++ pre/loopy_birds.rb
@@ -0,0 +1,28 @@
+puts "1 bird on a wire - ha ha ha!"
+
+# for n in 2..100
+#   puts "#{n} birds on a wire - ha ha ha!"
+# end
+
+# x = 2
+
+# while x < 10
+#   puts "#{x} birds on a wire - ha ha ha!"
+#   # The following is the same as saying x = x + 1
+#   x += 1
+# end
+
+# until x == 10
+#   puts "#{x} birds on a wire - ha ha ha!"
+#   x += 1
+# end
+
+# 5.times do |i|
+#   puts "#{i+2} birds on a wire - ha ha ha!"
+# end
+
+# 3.times do
+#   puts "She loves you, yeah yeah yeah!"
+# end
+
+2.upto(10){ |n| puts "#{n} birds on a wire - ha ha ha!" }
